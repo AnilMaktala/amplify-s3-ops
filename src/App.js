1384787b72abf4f00f11920ed3e476a4a8693c96
Anil Maktala <maktala.anil@gmail.com>
2023-07-27 21:43:53 -0400

    updated auth redirect url

--- src/App.js
+++ src/App.js
@@ -11,7 +11,37 @@ import TopNavigation, { TopNavigationProps } from "@cloudscape-design/components
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === "[::1]" ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
+);
 
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [
+  localRedirectSignIn,
+  productionRedirectSignIn,
+] = awsExports.oauth.redirectSignIn.split(",");
+
+const [
+  localRedirectSignOut,
+  productionRedirectSignOut,
+] = awsExports.oauth.redirectSignOut.split(",");
+
+const updatedAwsConfig = {
+  ...awsExports,
+  oauth: {
+    ...awsExports.oauth,
+    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+  }
+}
+
+Amplify.configure(updatedAwsConfig);
 
 
 function App() {
@@ -126,7 +156,7 @@ function App() {
     return menu
   }, [
     user,
-   Auth
+    Auth
   ])
 
   return (
