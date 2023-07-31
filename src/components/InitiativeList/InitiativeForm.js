b936a9f8c210636d2fe0d168fbf538e45efc441c
Anil Maktala <maktala.anil@gmail.com>
2023-07-31 11:02:00 -0400

    updated initiative form

--- src/components/InitiativeList/InitiativeForm.js
+++ src/components/InitiativeList/InitiativeForm.js
@@ -17,7 +17,7 @@ import {
 import SpaceBetween from "@cloudscape-design/components/space-between";
 import { Amplify, API, graphqlOperation } from 'aws-amplify'
 import { createInitiative } from '../../graphql/mutations'
-
+import { listPlans, listThemes } from "../../graphql/queries";
 function IntiativeForm({ setShowForm, trigger }) {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
@@ -25,16 +25,71 @@ function IntiativeForm({ setShowForm, trigger }) {
     const [themeID, setThemeID] = useState("");
     const [planID, setPlanID] = useState(1);
     const [status, setStatus] = useState(2);
+    const [
+        selectedPlan,
+        setSelectedPlan
+    ] = React.useState({ label: "select Plan", value: "0" });
+    const [
+        selectedTheme,
+        setSelectedTheme
+    ] = React.useState({ label: "select Theme", value: "0" });
+    const [plans, setPlans] = useState([]);
+    const [themes, setThemes] = useState([]);
     const [state, setState] = useState({
         title: "intiative2",
         description: "intiative2",
         rank: 3,
         themeID: 2,
         planID: 2,
+        bucket: "",
         status: "ACTIVE"
 
 
     });
+    const [
+        selectedStatus,
+        setSelectedStatus
+    ] = React.useState({ label: "select status", value: "0" });
+    const loadPlans = async () => {
+        const res = await API.graphql({
+            query: listPlans
+        });
+        console.log(res.data.listThemes);
+        res.data.listPlans.items.forEach((value) => {
+            themes.push({
+                key: value.id,
+                value: value.title,
+            });
+        });
+        // Update the options state
+        setPlans([
+            { key: 'Select a plan', value: '' },
+            ...themes
+        ])
+        //setAllItems(res.data.listPlans.items);
+    };
+    const loadThemes = async () => {
+        const res = await API.graphql({
+            query: listThemes
+        });
+        console.log(res.data.listThemes);
+        res.data.listThemes.items.forEach((value) => {
+            themes.push({
+                key: value.id,
+                value: value.title,
+            });
+        });
+        // Update the options state
+        setThemes([
+            { key: 'Select a theme', value: '' },
+            ...themes
+        ])
+        //setAllItems(res.data.listPlans.items);
+    };
+    const [
+        selectedBucket,
+        setSelectedBucket
+    ] = React.useState({ label: "Select Bucket", value: "0" });
     function handleChange() {
         // const name = evt.target.name;
 
@@ -48,11 +103,16 @@ function IntiativeForm({ setShowForm, trigger }) {
         state.title = title;
         state.description = description;
         state.rank = rank;
-        // state.themeID = themeID;
-        // state.planID = planID;
-        // state.status = status;
-
+        state.planID = selectedPlan.value;
+        state.themeID = selectedTheme.value;
+        state.bucket = selectedBucket.id;
+        state.status = selectedStatus.id;
+        console.log(state);
     };
+    useEffect(() => {
+        loadPlans();
+        loadThemes();
+    }, []);
 
     return (
 
@@ -111,12 +171,53 @@ function IntiativeForm({ setShowForm, trigger }) {
                             secondaryControl={<Button iconName="refresh" />}
                         >
                             <Select
+                                selectedOption={selectedTheme}
+                                onChange={({ detail }) =>
+                                    setSelectedTheme(detail.selectedOption)
+                                }
+                                options={themes}
+                            />
+                        </FormField>
+
+                        <FormField
+                            label="PlanID"
+                            secondaryControl={<Button iconName="refresh" />}
+                        >
+                            <Select
+                                selectedOption={selectedPlan}
+                                onChange={({ detail }) =>
+                                    setSelectedPlan(detail.selectedOption)
+                                }
+                                options={plans}
+                            />
+                        </FormField>
+                        <FormField
+                            label="Status"
+                        >
+                            <Select
+                                selectedOption={selectedStatus}
+                                onChange={({ detail }) =>
+                                    setSelectedStatus(detail.selectedOption)
+                                }
                                 options={[
-                                    { label: "sg-00dcd368", id: "1" },
-                                    { label: "sg-02dcd36a", id: "2" },
-                                    { label: "sg-04dcd36c", id: "3" },
-                                    { label: "sg-05fa4668", id: "4" },
-                                    { label: "sg-064a9062", id: "5" }
+                                    { label: "PROPOSED", id: "PROPOSED" },
+                                    { label: "ACTIVE", id: "ACTIVE" },
+                                    { label: "CANCELLED", id: "CANCELLED" }
+                                ]}
+                            />
+                        </FormField>
+                        <FormField
+                            label="Bucket"
+                        >
+                            <Select
+                                selectedOption={selectedBucket}
+                                onChange={({ detail }) =>
+                                    setSelectedBucket(detail.selectedOption)
+                                }
+                                options={[
+                                    { label: "TOPN", id: "TOPN" },
+                                    { label: "XORG", id: "XORG" },
+                                    { label: "CUT", id: "CUT" }
                                 ]}
                             />
                         </FormField>
