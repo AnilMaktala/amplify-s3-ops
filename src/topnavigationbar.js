20a65978b3f341b85ff441ead9022732cb009284
Anil Maktala <maktala.anil@gmail.com>
2023-08-03 17:12:51 -0400

    changed logo extension to jpeg

--- src/topnavigationbar.js
+++ src/topnavigationbar.js
@@ -1,33 +1,43 @@
-import React, { useMemo } from 'react';
-import { TopNavigation } from '@cloudscape-design/components';
-import logo from './s3+g.png'
+import React, { useMemo } from "react";
+import { TopNavigation } from "@cloudscape-design/components";
+import logo from "./s3+g.png";
 const TopNavigationBar = ({ user, handleUserProfileAction }) => {
-    const utilities = useMemo(() => {
-        const menu = [];
+  const utilities = useMemo(() => {
+    const menu = [];
 
-        if (user) {
-            menu.push({ type: 'button', text: 'Organizations', href: '/organizations', external: false });
-            menu.push({
-                type: 'menu-dropdown',
-                text: user.username.split('_')[1],
-                description: user.username.split('_')[1],
-                iconName: 'user-profile',
-                onItemClick: handleUserProfileAction,
-                items: [{ id: 'signout', text: 'Sign out' }],
-            });
-        }
+    if (user) {
+      menu.push({
+        type: "button",
+        text: "Organizations",
+        href: "/organizations",
+        external: false,
+      });
+      menu.push({
+        type: "menu-dropdown",
+        text: user.username.split("_")[1],
+        description: user.username.split("_")[1],
+        iconName: "user-profile",
+        onItemClick: handleUserProfileAction,
+        items: [{ id: "signout", text: "Sign out" }],
+      });
+    }
 
-        return menu;
-    }, [user]);
+    return menu;
+  }, [user]);
 
-    return <TopNavigation identity={{
-        href: '#',
-        title: 'Operational Planning',
+  return (
+    <TopNavigation
+      identity={{
+        href: "#",
+        title: "Operational Planning",
         logo: {
-            src: logo,
-            alt: "Service"
-        }
-    }} utilities={utilities} />;
+          src: logo,
+          alt: "Service",
+        },
+      }}
+      utilities={utilities}
+    />
+  );
 };
 
 export default TopNavigationBar;
